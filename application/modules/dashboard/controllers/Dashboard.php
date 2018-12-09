a5f6ece52163d73d86f91a0d8cba4fc9e831f67f
owsis <atowsis@gmail.com>
2018-12-09 10:21:47 +0700

    add upload doc_rapat and doc_absensi

--- application/modules/dashboard/controllers/Dashboard.php
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -6,24 +6,96 @@ class Dashboard extends MX_Controller {
 	public function __construct() 
 	{
 		parent::__construct();
-		$this->table = 'd003';
-		// $this->load->model('RuanganModel', 'ruangandb');
+		$this->table = 'em_d003';
+		$this->table_jadwal = 'em_d004';
+		$this->load->model('DashboardModel', 'dashboarddb');
+		$this->load->helper('form', 'url');
+		$this->load->library('form_validation');
 		// $this->load->helper('form');
-		if ($this->session->userdata('timeout') <= time()) {
-			redirect('http://eoffice.kemendesa.go.id');
-		}
+		// if ($this->session->userdata('timeout') <= time()) {
+		// 	redirect('http://eoffice.kemendesa.go.id');
+		// }
 	}
 
 	public function index()
 	{
-		$data['content_view'] = $this->load->view('dashboard', '', TRUE);
-		$data['menu'] = $this->load->view('main_layout/_base_sidebar', '', TRUE);
+		$this->db->where('nip', $this->session->userdata('nip'))->where('(doc_rapat IS NULL OR doc_absensi IS NULL)');
+		$data_dashboard['jadwal'] = $this->db->get($this->table_jadwal);
+		// $data_dashboard['jadwal'] = $this->db->get($this->table_jadwal)->result();
 
+		$data['content_view'] = $this->load->view('dashboard', $data_dashboard, TRUE);
+		$data['menu'] = $this->load->view('main_layout/_base_sidebar', '', TRUE);
+		$data['script_js'] = $this->load->view('dashboard-js', '', TRUE);
 		$data['additional_assets_css'] = array(
 			'css/style-hover.css'
 		);
 
 		$this->load->view('main_layout/_base_layout', $data);
+	}
+
+	public function upload_doc()
+	{
+		# code...
+	}
+
+	public function upload_doc_rapat($id)
+	{
+		date_default_timezone_set('Asia/Jakarta');
+		$new_name = time(). '-' .$_FILES["doc_rapat"]['name'];
+
+		$config['upload_path'] = './uploaded/dok/rapat/';
+		$config['allowed_types'] = 'pdf|jpg|png';
+		$config['max_size']  = '2048';
+		$config['file_name'] = $new_name;
+
+		$this->load->library('upload', $config);
+		$this->upload->initialize($config);
+
+		if (!$this->upload->do_upload('doc_rapat')) {
+
+			$error = array('error' => $this->upload->display_errors());
+			echo $this->upload->display_errors();
+
+		} else {
+
+			$upload_data = $this->upload->data();
+			$data['doc_rapat'] = $upload_data['file_name'];
+
+			$this->dashboarddb->update($this->table_jadwal, $data, 'id', $id);
+			redirect('/dashboard', 'refresh');
+
+		}
+	}
+
+	public function upload_doc_absensi($id)
+	{
+		date_default_timezone_set('Asia/Jakarta');
+		$new_name = time(). '-' .$_FILES["doc_absensi"]['name'];
+
+		$config['upload_path'] = './uploaded/dok/absensi/';
+		$config['allowed_types'] = 'pdf|jpg|png';
+		$config['max_size']  = '2048';
+		$config['file_name'] = $new_name;
+
+		$this->load->library('upload', $config);
+		$this->upload->initialize($config);
+
+		if (!$this->upload->do_upload('doc_absensi')) {
+
+			$error = array('error' => $this->upload->display_errors());
+			echo $this->upload->display_errors();
+
+		} else {
+
+			$upload_data = $this->upload->data();
+			$data['doc_absensi'] = $upload_data['file_name'];
+
+			$run = $this->dashboarddb->update($this->table_jadwal, $data, 'id', $id);
+			// print_r($run);
+
+			redirect('/dashboard', 'refresh');
+
+		}
 	}
 
 }
--- application/modules/dashboard/views/dashboard.php
+++ application/modules/dashboard/views/dashboard.php
@@ -36,3 +36,158 @@
 		</div>
 	</div>	
 </div>
+
+<div class="row">
+	<div class="col-md-12 grid-margin stretch-card">
+		<div class="card">
+			<div class="card-body">
+				<table id="dashboard-jadwal" class="table" style="width:100%">
+					<thead>
+						<tr>
+							<th>Dok. Agenda Rapat</th>
+							<th>Dok. Absensi</th>
+							<th>NIP</th>
+							<th>Judul</th>
+							<th>Deskripsi</th>
+							<th>Ruangan</th>
+							<th>Mulai Rapat</th>
+							<th>Selesai Rapat</th>
+							<th>Jumlah Peserta</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach($jadwal->result() as $val) {?>
+							<tr>
+								<?php if(!isset($val->doc_rapat)) {?>
+									<td>
+										<button class="btn btn-outline-info btn-sm" data-toggle="modal" data-target="#uploadRapat">UPLOAD</button>
+									</td>
+								<?php } else { ?>
+									<td><?=$val->doc_rapat?></td>
+								<?php } ?>
+								<?php if(!isset($val->doc_absensi)) {?>
+									<td>
+										<button class="btn btn-outline-info btn-sm" data-toggle="modal" data-target="#uploadAbsensi">UPLOAD</button>
+									</td>
+								<?php } else { ?>
+									<td><?=$val->doc_absensi?></td>
+								<?php } ?>
+								<td><?=$val->nip?></td>
+								<td><?=$val->title?></td>
+								<td><?=$val->desc?></td>
+								<td><?=$val->code_r?></td>
+								<td><?=$val->start?></td>
+								<td><?=$val->end?></td>
+								<td><?=$val->jumlah_peserta?></td>
+								
+							</tr>
+
+							<!-- MODAL UPLOAD RAPAT -->
+							<div class="modal fade" id="uploadRapat" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+								<div class="modal-dialog" role="document">
+									<div class="modal-content">
+										<form class="form-horizontal" action="<?=site_url('dashboard/upload_doc_rapat/' . $val->id)?>" method="POST" enctype="multipart/form-data" id="form_update">
+											<input type="hidden" name="calendar_id" value="0">
+											<div class="modal-header">
+												<h4 class="modal-title" id="myModalLabel">Agenda Dokumen Rapat</h4>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
+											</div>
+											<div class="modal-body">
+
+												<input type="hidden" name="id" class="form-control" readonly required>
+												<div class="form-group">
+													<div class="alert alert-danger" style="display: none;"></div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-sm-6">Judul<span class="required"> * </span></label>
+													<div class="col-sm-10">
+														<input type="text" name="nip" class="form-control" value="<?=$val->title?>"readonly required>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-sm-6">Deskripsi<span class="required"> * </span></label>
+													<div class="col-sm-10">
+														<input type="text" name="nip" class="form-control" value="<?=$val->desc?>"readonly required>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-sm-6">Dokumen Rapat</label>
+													<input type="file" name="doc_rapat" class="file-upload-default" id="upload-rapat">
+													<div class="input-group col-sm-10">
+														<input type="text" class="form-control file-upload-info" disabled placeholder="Dokumen Rapat maks. 2Mb">
+														<span class="input-group-append">
+															<button class="file-upload-browse btn btn-info" type="button">Cari</button>
+														</span>
+													</div>
+													<p class="text-danger col-sm-6" id="size-file-rapat" style="visibility: hidden;">Maksimal ukuran file 2Mb</p>
+												</div>
+
+											</div>
+											<div class="modal-footer" id="modal-footer">
+												<!-- <a href="javascript::void" class="btn default" data-dismiss="modal">Cancel</a> -->
+												<a href="" class="btn btn-danger" data-dismiss="modal">Batalkan</a>
+												<button id="submitRapat" type="submit" class="btn btn-success">Simpan</button>
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+
+							<!-- MODAL UPLOAD ABSENSI -->
+							<div class="modal fade" id="uploadAbsensi" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+								<div class="modal-dialog" role="document">
+									<div class="modal-content">
+										<form class="form-horizontal" action="<?=site_url('dashboard/upload_doc_absensi/' . $val->id)?>" method="POST" enctype="multipart/form-data" id="form_update">
+											<input type="hidden" name="calendar_id" value="0">
+											<div class="modal-header">
+												<h4 class="modal-title" id="myModalLabel">Absensi</h4>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
+											</div>
+											<div class="modal-body">
+
+												<input type="hidden" name="id" class="form-control" readonly required>
+												<div class="form-group">
+													<div class="alert alert-danger" style="display: none;"></div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-sm-6">Judul<span class="required"> * </span></label>
+													<div class="col-sm-10">
+														<input type="text" name="nip" class="form-control" value="<?=$val->title?>"readonly required>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-sm-6">Deskripsi<span class="required"> * </span></label>
+													<div class="col-sm-10">
+														<input type="text" name="nip" class="form-control" value="<?=$val->desc?>"readonly required>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-sm-6">Dokumen Absensi</label>
+													<input type="file" name="doc_absensi" class="file-upload-default" id="upload-absensi">
+													<div class="input-group col-sm-10">
+														<input type="text" class="form-control file-upload-info" disabled placeholder="Dokumen Absensi maks. 2Mb">
+														<span class="input-group-append">
+															<button class="file-upload-browse btn btn-info" type="button">Cari</button>
+														</span>
+													</div>
+													<p class="text-danger col-sm-6" id="size-file-absensi" style="visibility: hidden;">Maksimal ukuran file 2Mb</p>
+												</div>
+
+											</div>
+											<div class="modal-footer" id="modal-footer">
+												<!-- <a href="javascript::void" class="btn default" data-dismiss="modal">Cancel</a> -->
+												<a href="" class="btn btn-danger" data-dismiss="modal">Batalkan</a>
+												<button id="submitAbsensi" type="submit" class="btn btn-success">Simpan</button>
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+						<?php } ?>
+					</tbody>
+				</table>
+				
+			</div>
+		</div>
+	</div>	
+</div>
